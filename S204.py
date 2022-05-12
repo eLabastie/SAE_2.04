701a182f589e752b1fc407a0c7ba18ed5491d6df
eLabastie <99148033+eLabastie@users.noreply.github.com>
2022-05-12 19:12:49 +0200

    Contenu python este

--- S204.py
+++ S204.py
@@ -22,36 +22,31 @@ print('\n')
 print("thème 4 : Évolution des accidents des deux roues sur une période ")
 
 # Saisie du choix de l'utilisateur
-choix=input("Veuillez choisir un thème (1,2,3,4) : ")
-"""
-if choix==1:
 
-elif choix==2:
-"""
-elif choix==3:
+choix=input("Veuillez choisir un thème (1,2,3,4) : ")
+
+if choix=='1':
+    print("      Thème 1 : Accidents des deux roues selon l'état du sol")
     # Saisie des paramètres de la requête
-    print("Luminosités renseignées dans la base de données : Nuit éclairée, Nuit éclairée insuffisant et Nuit sans éclairage")
-    typeLum=input("Saisissez la luminosite à consulter (1,2,3,4) : ")
-    """
-SELECT HOUR(MDate.DateFormatStandard) as Heure, (MLuminosite.libelle_luminosite) as Luminosite, COUNT(MLuminosite.libelle_luminosite) as Nombre
-FROM MAccident
-INNER JOIN MDate on MAccident.date_id = MDate.date_id
-INNER JOIN MLuminosite on MAccident.lum_id = MLuminosite.code
-WHERE MLuminosite.libelle_luminosite = "Nuit eclairee"
-GROUP BY Heure
-"""
-"""
-elif choix==4:
+    Etat_sol=input("Veullez indiquer l'etat(s) du sol lors de l'accident \n 1:Humide, 2:Mouillee,  3:Enneigee, 4:Verglacee, 5:Gras boueux, 6:Gravillons, 7:Sec_Normal \n (1 OR 2 OR 5) : ")
+   
+
+elif choix=='2':
+    print("      Thème 2 : Accidents des deux roues selon la cause de l'accident ")
+     # Saisie des paramètres de la requête
+elif choix=='3':
+    print("      Thème 3 : Accidents des deux roues selon l'heure de la nuit ")
+    # Saisie des paramètres de la requête
+    
+
+
+elif choix=='4':
+     print("      Thème 4 : Évolution des accidents des deux roues sur une période ")
+    
     # Saisie des paramètres de la requête
     dateDeb=input("Choisissez la date de départ de la période à consulter (Entre 1984 et 1997) : ")
     dateFin=input("Choisissez la date de fin de la période à consulter (Entre 1984 et 1997) : ")
     
-    graph4=pandas.read_sql("select ", conn)
-    graph4.plot(x="Reference", y="QteStock")
+    graph4=p.read_sql("SELECT YEAR(MDate.DateFormatStandard) as Annee, COUNT(accident_id) as Nombre FROM MAccident INNER JOIN MDate ON MAccident.date_id = MDate.date_id WHERE impliq_id = 2 AND YEAR(MDate.DateFormatStandard) BETWEEN " + dateDeb + " AND " + dateFin + " GROUP BY Annee", conn)
+    graph4.plot( x="Annee", y="Nombre")
     plt.show()
-    """
-    """
-SELECT YEAR(MDate.DateFormatStandard) as Annee, COUNT(accident_id)
-FROM MAccident
-INNER JOIN MDate ON MAccident.date_id = MDate.date_id
-GROUP BY Annee"""

a2e5e97617f6942787f2e73e0989fb0fe0337c9a
eetchever004 <99280669+eetchever004@users.noreply.github.com>
2022-05-14 18:58:37 +0200

    thème 3 et 4 qui marchent pour de bon

--- S204.py
+++ S204.py
@@ -69,21 +69,21 @@ elif choix=="3":
     elif typeLum=="2":
         typeLum="Nuit eclairee insuffisant"
     elif typeLum=="3":
-        typeLum=="Nuit sans eclairage"
+        typeLum="Nuit sans eclairage"
             
     graph3=pandas.read_sql("SELECT HOUR(MDate.DateFormatStandard) as Heure, (MLuminosite.libelle) as Luminosite, COUNT(MLuminosite.libelle_luminosite) as Nombre FROM MAccident INNER JOIN MDate on MAccident.date_id = MDate.date_id INNER JOIN MLuminosite on MAccident.lum_id = MLuminosite.code WHERE impliq_id = 2 AND MLuminosite.libelle = \"" + typeLum + "\" AND HOUR(MDate.DateFormatStandard) NOT BETWEEN 9 AND 15 GROUP BY Heure", conn)
     graph3.plot(kind="bar", x="Heure", y="Nombre")
-    plt.show()    
+    plt.show()   
     
 
 
-elif choix=='4':
-     print("      Thème 4 : Évolution des accidents des deux roues sur une période ")
-    
+elif choix=="4":
     # Saisie des paramètres de la requête
-    dateDeb=input("Choisissez la date de départ de la période à consulter (Entre 1984 et 1997) : ")
-    dateFin=input("Choisissez la date de fin de la période à consulter (Entre 1984 et 1997) : ")
     
-    graph4=p.read_sql("SELECT YEAR(MDate.DateFormatStandard) as Annee, COUNT(accident_id) as Nombre FROM MAccident INNER JOIN MDate ON MAccident.date_id = MDate.date_id WHERE impliq_id = 2 AND YEAR(MDate.DateFormatStandard) BETWEEN " + dateDeb + " AND " + dateFin + " GROUP BY Annee", conn)
-    graph4.plot( x="Annee", y="Nombre")
+    dateDeb=saisieVerif(1984, 1997, "Choisissez la date de départ de la période à consulter (Entre 1984 et 1997) : ")
+    dateFin=saisieVerif(1984, 1997, "Choisissez la date de fin de la période à consulter (Entre 1984 et 1997) : ")
+    
+    #creation du graph
+    graph4=pandas.read_sql("SELECT YEAR(MDate.DateFormatStandard) as Annee, COUNT(accident_id) as Nombre FROM MAccident INNER JOIN MDate ON MAccident.date_id = MDate.date_id WHERE impliq_id = 2 AND YEAR(MDate.DateFormatStandard) BETWEEN " + dateDeb + " AND " + dateFin + " GROUP BY Annee", conn)
+    graph4.plot(x="Annee", y="Nombre")
     plt.show()
